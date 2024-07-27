1ff0bc109bb2cebc662db6cf7fdef0dde431a573
Jake Godsall <jake.edward.godsall@gmail.com>
2024-07-27 13:48:07 +0100

    added basic portfolio view function for displaying the webpage and urlpatterns

--- portfolio_site/portfolio/admin.py
+++ portfolio_site/portfolio/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Skill, Project
 
+
 class SkillAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
--- portfolio_site/portfolio/views.py
+++ portfolio_site/portfolio/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Project, Skill
 
-# Create your views here.
+
+# View to handle displaying the portfolio page
+def portfolio(request):
+    # Get the skill ID from the query parameters for filtering projects
+    skill_id = request.GET.get('skill')
+    # Filter projects by the selected skill if provided, otherwise get all projects
+    if skill_id:
+        projects = Project.objects.filter(skills__id=skill_id)
+    else:
+        projects = Project.objects.all()
+
+    # Get all skills to display in skills section
+    skills = Skill.objects.all()
+
+    # Render the portfolio page with projects, skills, and the contact form
+    return render(request, 'portfolio/portfolio.html', {
+        'projects': projects,
+        'skills': skills,
+    })
