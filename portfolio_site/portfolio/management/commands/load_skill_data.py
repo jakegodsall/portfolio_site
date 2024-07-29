0b02ca22f48ff7fbb84d39f82cec6e10c5dbad53
Jake Godsall <jake.edward.godsall@gmail.com>
2024-07-29 07:10:34 +0100

    modified load_skill_data to add exception handling and only bootstrap if table is empty

--- portfolio_site/portfolio/management/commands/load_skill_data.py
+++ portfolio_site/portfolio/management/commands/load_skill_data.py
@@ -9,9 +9,19 @@ class Command(BaseCommand):
     help = 'Load initial data into the Skill table from a JSON file'
 
     def handle(self, *args, **kwargs):
+        # Check if the Product table is empty
+        if Skill.objects.exists():
+            self.stdout.write(self.style.WARNING('The Skill table is not empty. Skipping bootstrap.'))
+            return
+
         file_path = Path(settings.BASE_DIR, 'data', 'bootstrap_skill.json')
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-            for skill in data:
-                Skill.objects.create(**skill)
-            self.stdout.write(self.style.SUCCESS('Successfully loaded initial Skills'))
+        try:
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                for skill in data:
+                    Skill.objects.create(**skill)
+                self.stdout.write(self.style.SUCCESS('Successfully loaded initial Skills'))
+        except FileNotFoundError:
+            self.stdout.write(self.style.ERROR(f'File not found: {file_path}'))
+        except json.JSONDecodeError:
+            self.stdout.write(self.style.ERROR('Invalid JSON format'))
