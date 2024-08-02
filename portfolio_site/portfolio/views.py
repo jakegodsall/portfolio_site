ea0be67c5f3c7f1782fc7d9a1ff3e39d5130c2d5
Jake Godsall <jake.edward.godsall@gmail.com>
2024-08-02 19:19:21 +0100

    implemented basic contact form logic

--- portfolio_site/portfolio/views.py
+++ portfolio_site/portfolio/views.py
@@ -1,9 +1,33 @@
+import os
+
 from django.shortcuts import render
 from .models import Project, Skill
+from .forms import ContactForm
+
+from django.core.mail import send_mail
 
 
 # View to handle displaying the portfolio page
 def portfolio(request):
+    # POST REQUEST FOR FORM
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            send_mail(
+                f"Message sent from {name}",
+                message,
+                email,
+                [os.getenv('CONTACT_EMAIL_ADDRESS')]
+            )
+    else:
+        form = ContactForm()
+
+    # GET REQUESTS
+
     # Get the skill ID from the query parameters for filtering projects
     skill_id = request.GET.get('skill')
     # Filter projects by the selected skill if provided, otherwise get all projects
