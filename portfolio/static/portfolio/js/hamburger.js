6af96aa5898ba121864c4567af90f223699eefc1
Jake Godsall <jake.edward.godsall@gmail.com>
2024-08-23 16:10:59 +0100

    extracted hamburger.js for mobile menu layout

---
+++ portfolio/static/portfolio/js/hamburger.js
@@ -0,0 +1,15 @@
+document.getElementById("hamburger").addEventListener("click", function(event) {
+     const mobileMenu = document.getElementById("mobile-menu");
+     const hamburger = document.getElementById("hamburger");
+     mobileMenu.classList.toggle("active");
+     hamburger.classList.toggle("active");
+})
+
+document.getElementById("mobile-menu").addEventListener("click", function(event) {
+    const mobileMenu = document.getElementById("mobile-menu");
+    const hamburger = document.getElementById("hamburger");
+    if (event.target === mobileMenu) {
+        mobileMenu.classList.remove("active");
+        hamburger.classList.remove("active");
+    }
+});
--- portfolio/static/portfolio/js/script.js
+++ portfolio/static/portfolio/js/script.js
@@ -1,19 +1,3 @@
-document.getElementById("hamburger").addEventListener("click", function(event) {
-     const mobileMenu = document.getElementById("mobile-menu");
-     const hamburger = document.getElementById("hamburger");
-     mobileMenu.classList.toggle("active");
-     hamburger.classList.toggle("active");
-})
-
-document.getElementById("mobile-menu").addEventListener("click", function(event) {
-    const mobileMenu = document.getElementById("mobile-menu");
-    const hamburger = document.getElementById("hamburger");
-    if (event.target === mobileMenu) {
-        mobileMenu.classList.remove("active");
-        hamburger.classList.remove("active");
-    }
-});
-
 document.getElementById('explore-btn').addEventListener('click', function(event) {
     event.preventDefault();
     document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
