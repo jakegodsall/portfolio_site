1530731b50a0faa9791891273f26ad457b27c303
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-16 12:38:26 +0100

    removed whitenoise dep

--- portfolio_site/settings/base.py
+++ portfolio_site/settings/base.py
@@ -116,7 +116,6 @@ STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = [
     BASE_DIR / "portfolio" / "staticfiles",
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
