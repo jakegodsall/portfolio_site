ed38475ae6eb56cb77f6ef04ff74a9ad80b212dc
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-16 11:57:54 +0100

    changed the media location in s3

--- portfolio_site/settings/production.py
+++ portfolio_site/settings/production.py
@@ -24,7 +24,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # For media files
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 # Database
 
