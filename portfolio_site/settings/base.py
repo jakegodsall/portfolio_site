cfc0570ef34f13893c4e556a7fb2cb7f435a4337
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-25 16:53:25 +0100

    migrated db for anki app

--- portfolio_site/settings/base.py
+++ portfolio_site/settings/base.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio'
+    'portfolio',
+    'anki_decks'
 ]
 
 MIDDLEWARE = [
