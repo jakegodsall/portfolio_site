772dc57fe1d699b153ff64139ba19047f2dbe391
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-26 18:40:33 +0100

    made fields readonly in admin site for flashcard deck

--- anki_decks/admin.py
+++ anki_decks/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import FlashcardDeck
 
-admin.site.register(FlashcardDeck)
+
+class FlashcardDeckAdmin(admin.ModelAdmin):
+    readonly_fields = ['name', 'deck', 'most_recent_upload', 'number_of_uploads']
+
+
+admin.site.register(FlashcardDeck, FlashcardDeckAdmin)
