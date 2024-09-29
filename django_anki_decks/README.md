b0a588dda146a88a5eda9784000aae798a5481a6
Jake Godsall <jake.edward.godsall@gmail.com>
2024-09-29 17:57:56 +0100

    Removed anki_decks app from the main repository

--- django_anki_decks/README.md
+++
@@ -1,1 +0,0 @@
-# django-anki-decks
--- django_anki_decks/anki_decks/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import FlashcardDeck
-
-def list_exported_decks(request):
-    decks = FlashcardDeck.objects.all()
-
-    return render(request, 'anki_decks/flashcard_decks.html', {'decks': decks})
